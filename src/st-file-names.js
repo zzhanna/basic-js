894b42bf0854d249f220206fc538b2ece9d00e2a
zzhanna <foto1drug@gmail.com>
2021-09-29 20:21:33 +0200

    feat: done email and file-names

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -15,7 +15,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+    for (let i = 0; i < names.length; i++) {
+    let k = 1;
+    for (let j = i + 1; j < names.length; j++) {
+      if (names[i] === names[j]) {
+        names[j] = `${names[j]}(${k++})`;
+      }
+    }
+  }
+  return names;
 }
