37588db2197ea751c617ac3afc811ef67f935c16
zzhanna <foto1drug@gmail.com>
2021-09-29 18:36:07 +0200

    feat: done st-encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,19 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let arr = [];
+  let ind = 1;
+     for (let i=0; i < str.length; i++) {
+      if (str[i] === str [i+1]) {
+        ind++ ;
+              } else {
+        ind!==1 ? arr.push([ind,str[i]].join('')) : arr.push([str[i]].join(''));
+        ind = 1;
+        
+   }
+      }
+
+         return arr.join('').toString();
 }
+
